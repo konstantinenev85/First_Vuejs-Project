0505549202b8d03477d242bc8d2d4bade91c179e
Konstantin <konstantinenev85@gmail.com>
2020-11-18 14:36:40 +0200

    First commit

---
+++ first-vuejs-project/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search...">
+      </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Sliven, BG</div>
+          <div class="date">Wednesday 18 November 2020</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°C</div>
+          <div class="weather">Sunny</div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      api_key: 'e4bd67dbf12b63e3317794e7531228bb'
+
+    }
+  } 
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Montserrat', sans-serif;
+}
+
+#app {
+  background-image: url('./assets/cold-background.jpg');
+  background-size: cover;
+  background-position: bottom;
+}
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+</style>
